5d595fbe56abda5abbcba25fec5469dd873eebc1
Leon Marin <lealmafor@gmail.com>
2025-11-17 22:42:58 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-    base: '/fandeco3d/',
-    plugins: [react(), tailwindcss()],
-=======
-  base: '/fandeco3d/',
-  plugins: [react(),tailwindcss()],
+   plugins: [react(),tailwindcss()],
 
->>>>>>> 566b2022c26b3d9d041f7f97390c06c61042545d
 })
